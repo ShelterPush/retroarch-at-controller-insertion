4648d0f229501e953d710d67def020a09012d39b
ShelterPush <38357508+ShelterPush@users.noreply.github.com>
2018-04-13 12:23:21 -0400

    Update mario.sh

--- mario.sh
+++ mario.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# Check if the script actually launches
 echo "Mario" >> /tmp/mariocheck.tmp
+# Require that this script only run if retroarch isn't already running (probably redundant, but it works)
 if ! pgrep -x "retroarch" > /dev/null
 then
 	retroarch -f -L /usr/lib/libretro/mupen64plus_libretro.so ~/Mario\ Kart\ 64.n64
